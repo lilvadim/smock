ceffe29dbec1bb93c25686db4f918301266d6b20
lilvadim <vadimmos2002@gmail.com>
2023-03-22 02:50:07 +0700

    null

--- src/main/kotlin/smock/external/cglib/CglibMockInterceptor.kt
+++ src/main/kotlin/smock/external/cglib/CglibMockInterceptor.kt
@@ -2,7 +2,6 @@ package smock.external.cglib
 
 import net.sf.cglib.proxy.MethodInterceptor
 import net.sf.cglib.proxy.MethodProxy
-import smock.exception.SmockException
 import smock.internal.AbstractInterceptor
 import smock.internal.CallData
 import smock.internal.CallValuesStorage
@@ -11,10 +10,9 @@ import java.lang.reflect.Method
 class CglibMockInterceptor(
     callValuesStorage: CallValuesStorage
 ) : AbstractInterceptor(callValuesStorage), MethodInterceptor {
-    override fun intercept(obj: Any, method: Method?, args: Array<out Any>?, proxy: MethodProxy?): Any {
+    override fun intercept(obj: Any, method: Method?, args: Array<out Any>?, proxy: MethodProxy?): Any? {
         val callData = CallData(obj, method, args?.toList() ?: emptyList())
         callValuesStorage.lastCall = callData
         return callValuesStorage.registeredAction(callData)?.invoke()
-            ?: throw SmockException("Method ${obj::class.qualifiedName}::${method?.name} is not smocked!")
     }
 }
